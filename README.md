509ba593c9c9338f272df87e49ccff265026db6f
Web Developer <useraccount@WebDeveloperMBP.local>
2022-06-29 16:09:12 -0500

    Version 1 - LM

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 # Figure
 
-App Description
+## What This App Does
+Renders an `<picture>` and `<img>` tag inside a `<figure>` tag with an optional `<figcaption>` tag to aid in SEO.
+
+The "imgSet" object defines both a "desktop" and "mobile" image source for optimizing download speeds for different devices.
+
+## VTEX Block Example
+
+```
+"figure#bicycle-photo-1": {
+    "props": {
+      "imgSet": {
+        "desktop": "absolute-path-to-desktop-image.jpg",
+        "mobile": "absolute-path-to-mobile-image.jpg"
+      },
+      "imgSrc": "absolute-path-to-default-image.jpg",
+      "caption": "The text you want below the image",
+      "alt": "Alt and Title Text"
+    }
+  }
+```
+
+## Required Props
+- imgSrc - This is the fallback / default image source. Must be absolute path.
+
+## Optional Props
+- imgSet - Desktop optimized image source and Mobile optimized image source. Breakpoint between Desktop and Mobile is 1025px.
+
+- caption - Caption for the image. Renders in a grey / white box beneath the image by default.
+
+- alt - Text here will render in the `<img>` 'alt' attribute and 'title' attribute.
+
+## Dom Example
+```
+<div class="eriksbikeshop-figure-1-x-figureContainer">
+    <div class="eriksbikeshop-figure-1-x-figureWrapper">
+        <figure class="eriksbikeshop-figure-1-x-figureTag">
+            <picture>
+                <source media="(min-width:1026px)" srcset="absolute-path-to-desktop.jpg">
+                <source media="(max-width:1025px)" srcset="absolute-path-to-mobile.jpg">
+                <img src="absolute-path-to-default.jpg" alt="Alt Text" title="Alt Text" class="eriksbikeshop-figure-1-x-figureImage" crossorigin="anonymous">
+                </picture>
+            <figcaption class="eriksbikeshop-figure-1-x-figureCaption">
+            <span class="eriksbikeshop-figure-1-x-figureCaptionText">
+                Caption Text
+            </span>
+            </figcaption>
+        </figure>
+    </div>
+</div>
+```
--- react/Figure.tsx
+++ react/Figure.tsx
@@ -4,21 +4,96 @@ import React from 'react';
 import styles from "./styles.css";
 
 interface FigureProps {
-
+  imgSet: imgSetObject
+  imgSrc: string
+  link?: linkObject
+  caption: string
+  alt: string
+  blockClass: string
 }
 
-const Figure: StorefrontFunctionComponent<FigureProps> = ({ }) => {
+interface imgSetObject {
+  desktop: string
+  mobile: string
+}
+
+interface linkObject {
+  url: string
+  newTab: Boolean
+}
+
+const Figure: StorefrontFunctionComponent<FigureProps> = ({ imgSet, imgSrc, caption, alt, link, blockClass }) => {
+
+  const blockClassFormat = blockClass ? `--${blockClass}` : "";
+
+  const imageReturn = (
+    <>
+      {imgSet &&
+        <picture>
+          <source media="(min-width:1026px)" srcSet={imgSet.desktop} />
+          <source media="(max-width:1025px)" srcSet={imgSet.mobile} />
+          <img src={imgSrc || imgSet.desktop} alt={alt} title={alt} className={blockClass ? `${styles.figureImage} ${styles.figureImage}${blockClassFormat}` : `${styles.figureImage}`} />
+        </picture>
+      }
+      {!imgSet &&
+        <img src={imgSrc} alt={alt} title={alt} className={blockClass ? `${styles.figureImage} ${styles.figureImage}${blockClassFormat}` : `${styles.figureImage}`} />
+      }
+    </>
+  )
+
+  if (!imgSrc) return <></>
 
   return (
-    <h3>Hello World!</h3>
+    <div className={blockClass ? `${styles.figureContainer} ${styles.figureContainer}${blockClassFormat}` : `${styles.figureContainer}`}>
+      <div className={blockClass ? `${styles.figureWrapper} ${styles.figureWrapper}${blockClassFormat}` : `${styles.figureWrapper}`}>
+        <figure role="img" aria-label={alt} className={blockClass ? `${styles.figureTag} ${styles.figureTag}${blockClassFormat}` : `${styles.figureTag}`}>
+          {link && link ? <a href={link.url} target={link.newTab ? "_blank" : "_self"} rel="noreferrer">{imageReturn}</a> : <>{imageReturn}</>}
+          {caption && <figcaption className={blockClass ? `${styles.figureCaption} ${styles.figureCaption}${blockClassFormat}` : `${styles.figureCaption}`}>
+            <span className={blockClass ? `${styles.figureCaptionText} ${styles.figureCaptionText}${blockClassFormat}` : `${styles.figureCaptionText}`}>{caption}</span>
+          </figcaption>}
+        </figure>
+      </div>
+    </div>
   )
 }
 
 Figure.schema = {
-  title: 'editor.hoverfeature.title',
-  description: 'editor.hoverfeature.description',
+  title: 'Image Figure',
+  description: 'Renders Figure Tag with optional caption for better SEO.',
   type: 'object',
-  properties: {}
+  properties: {
+    imgSrc: {
+      title: "Default Image Source",
+      description: "REQUIRED - Absolute Path to default image.",
+      type: "string"
+    },
+    imgSet: {
+      type: "object",
+      title: "Image Set",
+      properties: {
+        desktop: {
+          title: "Desktop Image Source",
+          description: "Optional - Image optimized for desktop devices.",
+          type: "string"
+        },
+        mobile: {
+          title: "Mobile Image Source",
+          description: "Optional - Image optimized for mobile devices.",
+          type: "string"
+        }
+      }
+    },
+    caption: {
+      title: "Image Caption",
+      description: "Optional - Text rendered below the image for SEO boost. No Markdown accepted.",
+      type: "string"
+    },
+    alt: {
+      title: "Alt Text",
+      description: "Optional - Used for Alt, Title and ARIA attributes for screen readers and SEO boost. No Markdown accepted.",
+      type: "string"
+    }
+  }
 }
 
 export default Figure;
